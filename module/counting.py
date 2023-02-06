17165b74044617fe3a4f2661725886397e8c71ab
DESKTOP-CO6J3MI\roboc <robocon8a@gmail.com>
2023-02-07 00:22:48 +0700

    Refactor source code

--- module/counting.py
+++ module/counting.py
@@ -2,113 +2,111 @@ import os
 import numpy as np
 import cv2
 import datetime
+
+
 class Counting:
     def __init__(self, path_in, opening_sign):
-
-        self.opening_sign = opening_sign
+        self._path_out = "/media/Z/TrungNT108/ComputerVision_HUST/processed_image"
         self.path_in = path_in
-        self.path_out = '/media/Z/TrungNT108/ComputerVision_HUST/processed_image'
-
-    def create_folder(self, path_out):
-        if not os.path.exists(path_out):
-            os.mkdir(path_out)
+        self.opening_sign = opening_sign
 
     def get_shape(self, img):
-        height = img.shape[0]
-        width = img.shape[1]
-        channels = img.shape[2]
+        return img.shape[0], img.shape[1]
 
-        return height, width
+    def create_path_out(self):
+        if not os.path.exists(self._path_out):
+            os.mkdir(self._path_out)
 
     def apply_adaptive_threshold(self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        thresh_adaptive = 180
-        kernelSize = 27
-        C = -10
-        img_adapt_thresh = cv2.adaptiveThreshold(gray,thresh_adaptive,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,kernelSize, C)
-        # img_adapt_thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-        #     cv2.THRESH_BINARY,11,2)
-        return img_adapt_thresh
+        # Using thresh_adaptive = 180, kernel_size = 27, C = -10
+        adaptive_threshold = cv2.adaptiveThreshold(
+            gray,
+            180,
+            cv2.ADAPTIVE_THRESH_MEAN_C,
+            cv2.THRESH_BINARY,
+            27,
+            -10,
+        )
+        return adaptive_threshold
 
     def apply_opening(self, img):
-        if self.opening_sign == '0':
-            kernelSize = (3, 3)
+        if self.opening_sign == "0":
+            kernel_size = (3, 3)
         else:
-            kernelSize = (7, 9)
+            kernel_size = (7, 9)
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, kernelSize)
-        # print(kernel)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, kernel_size)
         opening_img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
         return opening_img
 
-    def extract_contour(self, img):
-        extracted_contours = []
+    def save_img(self, img):
+        # Saving
+        name_list = self.path_in.split("/")
+        path_out_png = (
+            self._path_out
+            + "/"
+            + str(datetime.datetime.now().timestamp())
+            + name_list[-1]
+        )
+        cv2.imwrite(path_out_png, img)
+        return path_out_png
+
+    def extract_contour_from_img(self, img):
+        contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         threshold = 0
-        contours, _ = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        extracted_contours = []
 
         for item in contours:
             area = cv2.contourArea(item)
-            if area > threshold:
+            if threshold < area:
                 extracted_contours.append(item)
 
         return extracted_contours
 
-    
-    def saving(self, img):
-        # Saving
-        name_list = self.path_in.split("/")
-        name_png = name_list[-1]
-        path_out_png = self.path_out + "/" + str(datetime.datetime.now().timestamp()) + name_png
-        cv2.imwrite(path_out_png, img)
-        return path_out_png
-
+    def process(self):
+        img = cv2.imread(self.path_in)
+        # Apply adaptive thresholding
+        adaptive_img = self.apply_adaptive_threshold(img)
+        # Apply opening operation
+        opening_img = self.apply_opening(adaptive_img)
+        # Getting contours
+        extracted_contours = self.extract_contour_from_img(opening_img)
+        # Save
+        path_adaptive = self.save_img(adaptive_img)
+        path_opening = self.save_img(opening_img)
+        return (path_adaptive, path_opening, extracted_contours)
 
     def counting_object(self):
         # Create destination folder
-        self.create_folder(self.path_out)
+        self.create_path_out()
 
-        img = cv2.imread(self.path_in)
-
-        h_img, w_img = self.get_shape(img)
-
-        # # Apply adaptive thresholding
-        adaptive_img = self.apply_adaptive_threshold(img)
-        path_adaptive = self.saving(adaptive_img)
-        # cv2.imshow("Applying adaptive thresholding",adaptive_img)
-
-        # # Apply opening operation
-        opening_img = self.apply_opening(adaptive_img)
-        path_opening = self.saving(opening_img)
-
-        # Getting contours
-        extracted_contours = self.extract_contour(opening_img)
-
-        # list = array
+        path_adaptive, path_opening, extracted_contours = self.process()
 
         # Visualize bounding box and contours
         for item in extracted_contours:
             # Rectangle
-            x,y,w,h = cv2.boundingRect(item)
+            x, y, w, h = cv2.boundingRect(item)
             rect = cv2.minAreaRect(item)
+            area_contour = cv2.contourArea(item)
             box = cv2.boxPoints(rect)
             box = np.int0(box)
             area_rec = cv2.contourArea(box)
-            area_contour = cv2.contourArea(item)
-            # print(area_contour/area_rec)
-            # if area_rec > 1000:
-            if area_contour/area_rec < 10:
 
-            # Large rectangle
-                img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-                
+            if 10 > area_contour / area_rec:
+                # Large rectangle
+                img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
                 # Bounding contour
-                img = cv2.drawContours(img,[item],0,(255,0,0),2)
+                img = cv2.drawContours(img, [item], 0, (255, 0, 0), 2)
 
                 # Small rectangle
-                img = cv2.drawContours(img,[box],0,(0,255,255),2)
+                img = cv2.drawContours(img, [box], 0, (0, 255, 255), 2)
 
-        return { 'denoise_image': self.path_in,
+        return {
+            "denoise_image": self.path_in,
             "path_adaptive": path_adaptive,
-            'path_opening': path_opening,
-            'output_image': self.saving(img),
-            'total_contours': len(extracted_contours)}
+            "path_opening": path_opening,
+            "output_image": self.saving(img),
+            "total_contours": len(extracted_contours),
+        }
--- module/denoise.py
+++ module/denoise.py
@@ -3,92 +3,86 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from math import log
-from statistics import mean
 
 
 class Denoise:
-    def __init__(self, path_in):
+    def __init__(self, path_in: str):
+        self._path_out = "denoise"
         self.path_in = path_in
-        self.path_out = 'denoise'
-        # pass
 
-    def create_folder(self, path_out):
-        if not os.path.exists(path_out):
-            os.mkdir(path_out)
-
-    def local_maximum(self, spec,win_size,thresh):
-        wid, hei = spec.shape
-        wid_loop_times = int(wid/win_size)
-        hei_loop_times = int(hei/win_size)
+    def local_maximum(self, spec, window_size, threshold):
+        w, h = spec.shape
         global_max = np.max(spec)
         max_pos = []
-        for i in range(wid_loop_times):
-            for j in range(hei_loop_times):
-                window = spec[win_size*i:win_size*(i+1), win_size*j:win_size*(j+1)]
+        w_loop_times = int(w / window_size)
+        h_loop_times = int(h / window_size)
+
+        for i in range(w_loop_times):
+            for j in range(h_loop_times):
+                window = spec[
+                    window_size * i : window_size * (i + 1),
+                    window_size * j : window_size * (j + 1),
+                ]
                 local_max = np.max(window)
-                if (local_max > (global_max*thresh) and local_max < global_max):
+                if local_max < global_max and local_max > (global_max * threshold):
                     ind = np.unravel_index(np.argmax(window, axis=None), window.shape)
-                    max_pos.append((5*i+ind[0], 5*j+ind[1]))
+                    max_pos.append((5 * i + ind[0], 5 * j + ind[1]))
+
         return max_pos
 
-    def denoise(self):
+    def create_path_out(self):
+        if not os.path.exists(self._path_out):
+            os.mkdir(self._path_out)
 
-        # Create path_out folder
-        self.create_folder(self.path_out)
-        
-        # Get path_out for image to save
-        name_list = self.path_in.split("/")
-        name_png = name_list[-1]
-        path_out_png = '/media/Z/TrungNT108/ComputerVision_HUST/processed_image' + "/" + name_png
-        
+    def get_correct_gamma(self):
         # Reading input image
         img = cv2.imread(self.path_in)
-        # cv2.imshow("Original image",img)
 
         # Filter noise and convert to gray image
-        median = cv2.medianBlur(img,5)
+        median = cv2.medianBlur(img, 5)
         gray = cv2.cvtColor(median, cv2.COLOR_BGR2GRAY)
-        
-        # Using gamma correction
-        gamma = log(img.mean())/log(128)
-        # print(gamma)
-        gamma_corrected = np.array(255*(gray / 255) ** gamma, dtype = 'uint8')
 
-        # convert image to floats and do dft saving as complex output
-        dft = cv2.dft(np.float32(gamma_corrected), flags = cv2.DFT_COMPLEX_OUTPUT)
+        # Using gamma correction
+        gamma = log(img.mean()) / log(128)
+        return np.array(255 * (gray / 255) ** gamma, dtype="uint8")
+
+    def shift_dft(self, dft):
         # apply shift of origin from upper left corner to center of image
         dft_shift = np.fft.fftshift(dft)
+        magnitude_spectrum = 20 * np.log(
+            cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1])
+        )
 
-        magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
-        max_pos = self.local_maximum(magnitude_spectrum, 5, 0.9)
-
-        # remove sin noise
         max_pos = self.local_maximum(magnitude_spectrum, 5, 0.9)
         for pos in max_pos:
-            # remove sin noise
             dft_shift[pos] = 0
 
         # shift origin from center to upper left corner
-        back_ishift = np.fft.ifftshift(dft_shift)
+        return np.fft.ifftshift(dft_shift)
+
+    def denoise(self):
+        # Init path
+        self.create_path_out()
+        name_list = self.path_in.split("/")
+        path_out_png = (
+            "/media/Z/TrungNT108/ComputerVision_HUST/processed_image"
+            + "/"
+            + name_list[-1]
+        )
+
+        # convert image to floats and do dft saving as complex output
+        corrected_gamma = self.get_correct_gamma()
+        dft = cv2.dft(np.float32(corrected_gamma), flags=cv2.DFT_COMPLEX_OUTPUT)
+        back_ishift = self.shift_dft(dft)
 
         # do idft saving as complex output
         img_back = cv2.idft(back_ishift)
 
         # combine complex components into original image again
-        img_back = cv2.magnitude(img_back[:,:,0], img_back[:,:,1])
-        
-        # dimensions = img_back.shape
-        # height = img.shape[0]
-        # width = img.shape[1]
-        # channels = img.shape[2]
-        
-        img_back[:,-1] = img_back[:,-2]
-        img_back[-1,:] = img_back[-2,:]
-        
-        plt.imsave(path_out_png, img_back, cmap='gray')
-        
+        img_back = cv2.magnitude(img_back[:, :, 0], img_back[:, :, 1])
+        img_back[:, -1] = img_back[:, -2]
+        img_back[-1, :] = img_back[-2, :]
+
+        plt.imsave(path_out_png, img_back, cmap="gray")
+
         return path_out_png
-        
-
-
-
